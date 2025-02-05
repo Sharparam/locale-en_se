f3db17ca8fc0b6670234ca8603928ab096e46b1e
Adam Hellberg <sharparam@sharparam.com>
2025-02-06 00:39:12 +0100

    Use postPatch instead of preBuild

--- package.nix
+++ package.nix
@@ -8,15 +8,15 @@
 )
 .overrideAttrs (
   final: prev: {
-    preBuild =
-      ''
+    postPatch =
+      prev.postPatch
+      + ''
         echo 'HACK: Adding en_SE as a supported locale to glibc'
         set -x
         dest=../glibc-2*/localedata/locales
-        cp -v ${lib.escapeShellArg "${./en_SE}"} $dest/locales/en_SE
-        echo 'en_SE.UTF-8/UTF-8 \' >> ../glibc-2*/localedata/SUPPORTED
+        cp -v ${lib.escapeShellArg "${./en_SE}"} localedata/locales/en_SE
+        echo 'en_SE.UTF-8/UTF-8 \' >> localedata/SUPPORTED
         set +x
-      ''
-      + prev.preBuild;
+      '';
   }
 )

7491cd1885054c0c2cdceaf998d2bc9d73ccf79e
Adam Hellberg <sharparam@sharparam.com>
2025-02-06 00:34:20 +0100

    Echo commands

--- package.nix
+++ package.nix
@@ -11,9 +11,11 @@
     preBuild =
       ''
         echo 'HACK: Adding en_SE as a supported locale to glibc'
+        set -x
         dest=../glibc-2*/localedata/locales
         cp -v ${lib.escapeShellArg "${./en_SE}"} $dest/locales/en_SE
         echo 'en_SE.UTF-8/UTF-8 \' >> ../glibc-2*/localedata/SUPPORTED
+        set +x
       ''
       + prev.preBuild;
   }

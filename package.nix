4b3212bd9290f8021cbb410fb8388160307358a8
Adam Hellberg <sharparam@sharparam.com>
2025-02-06 00:41:46 +0100

    Remove old code

--- package.nix
+++ package.nix
@@ -12,11 +12,8 @@
       prev.postPatch
       + ''
         echo 'HACK: Adding en_SE as a supported locale to glibc'
-        set -x
-        dest=../glibc-2*/localedata/locales
         cp -v ${lib.escapeShellArg "${./en_SE}"} localedata/locales/en_SE
         echo 'en_SE.UTF-8/UTF-8 \' >> localedata/SUPPORTED
-        set +x
       '';
   }
 )

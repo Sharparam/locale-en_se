df1c13f7e09be5567d7cd95c7e9c5e876706faef
Adam Hellberg <sharparam@sharparam.com>
2025-02-06 00:18:39 +0100

    Fix filename issue

--- package.nix
+++ package.nix
@@ -11,7 +11,7 @@
     preBuild =
       ''
         echo 'HACK: Adding en_SE as a supported locale to glibc'
-        cp -v ${lib.escapeShellArg "${./en_SE}"} ../glibc-2*/localedata/locales
+        cp -v ${lib.escapeShellArg "${./en_SE}"} ../glibc-2*/localedata/locales/en_SE
         echo 'en_SE.UTF-8/UTF-8 \' >> ../glibc-2*/localedata/SUPPORTED
       ''
       + prev.preBuild;
